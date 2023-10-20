4618d558b494bdde16a94f09ef04683442618882
sivanandam <sivanandam1000@gmail.com>
2023-10-20 14:25:43 +0530

    logic file updated

--- lib/logicprogeam.dart
+++ lib/logicprogeam.dart
@@ -1,6 +1,6 @@
-main(){
+main() {
   String str = 'madam';
-  // checkPalindrome(str);
+  checkPalindrome(str);
 
   //input  = 'aabbcdfggiiim'
   //output = a2b2c1d1f1g2i3m1
@@ -8,14 +8,40 @@ main(){
   // countCharacters(sample);
 
   //2nd largest in given array
-  List<int> sampleList = [6,10,35,21,98,21,44];
-  secondLargestInGivenArray(sampleList);
+  // List<int> sampleList = [6, 10, 35, 21, 98, 21, 44];
+  // secondLargestInGivenArray(sampleList);
+
+  // List<int> nums = [1, 2, 3, 5, 6, 8, 9];
+  // List<int> sizeList = [1,2,3,5,6,8,7,8];
+
+  /**
+   * [1,2,3,5,6,8,7,8];
+   * (1,2),(2,3), (5,6),(8) (7,8) = 5 pairs
+   */
+  // checkTheMatchingSizePairs(sizeList);
+
+  /**
+   * check given number is palindrome or not
+   */
+
+   // checkGivenNumberIsPalindromeOrNot(1221);
+}
+
+void checkTheMatchingSizePairs(List<int> sizeList) {
+  int count = 0;
+  for(int i = 0 ; i < sizeList.length-1; i++){
+    if(sizeList[i] + 1 == sizeList[i+1]){
+      count++;
+    }
+  }
+  print(count);
 }
 
 
 void checkPalindrome(String str){
   String reversed='';
-  for(int i = str.length-1; i >= 0;i--){
+  for(int i = str.length -1 ; i >= 0;i--){
+    print(i);
     reversed = '$reversed${str[i]}' ;
   }
   if(str == reversed){
@@ -59,5 +85,92 @@ void secondLargestInGivenArray(List<int> sampleArray) {
   print(' The Second largest number is $max2');
   print(' The Third largest number is $max3');
 }
+
+checkGivenNumberIsPalindromeOrNot(int num){
+
+  // need to reverse the number
+  // int numberToReverse = 353;
+  int reversedNumber = 0;
+  int beforeReverse = num;
+  while (num > 0) {
+    int reminder = num % 10;
+    reversedNumber = (reversedNumber * 10) + reminder;
+    num = (num / 10).toInt();
+  }
+  if (beforeReverse == reversedNumber) {
+    print("Number is Palindrome");
+  } else {
+    print("Number is not Palindrome");
+  }
+
+  print('The reverse number is $num');
+
+}
+
+/**
+ * List duplicate find logic
+ */
+/*List<int> dup = [1,2,3,4,4,5,8];
+
+  List<String> dupstr = ['sample','2','3','4','4','sample','8'];
+  List<String> orgi=[];
+
+  String abc = "ababcmycfiyjkllttreec";
+
+  orgi = dupstr.toSet().toList();
+
+  for(int i = 0 ; i < abc.length; i++){
+    for(int j = i+1 ; j < abc.length; j++){
+       if(abc[i] == abc[j]){
+         if(!orgi.contains(abc[i])){
+            orgi.add(abc[i]);
+
+         }
+       }
+
+    }
+  }
+  print(orgi);*/
+
+
+/**
+ * swap numbers logic with out and with temp
+ */
+
+
+/*int a = 34;
+  int b = 43;
+  int c = 77;
+  int temp;
+  temp = c;  // temp = 77
+  c =  b; // c = 43
+  b = a ; // b = 34
+  a = temp; // a = 77
+
+    int c = b;
+    b = a;
+    a = c;
+
+
+     int c = 57;
+    int d = 22;
+    int c = 60;
+
+    a = a + b; //10 + 20 = 30;
+    b = a - b; // 30 -  20 = 10;
+    a = a - b; //30 - 10 = 20
+
+    a = a + b + c + d; // 60
+    b = a - ( b + c + d);
+    c = a - ( b + c + d);
+    d = a - ( b + c + d);
+    a = a - ( b + c + d);
+
+
+
+
+  print('Sawpped values $a $b $c');*/
+
+
 
 
